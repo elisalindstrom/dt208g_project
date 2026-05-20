d740ecc88adb46a707c007bba4102c6e228504ab
elisalindstrom <elisalindstroem@gmail.com>
2026-05-20 19:16:25 +0200

    lagt till routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Courses } from './pages/courses/courses';
+import { Schedule } from './pages/schedule/schedule';
+import { NotFound } from './pages/not-found/not-found';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'home', component: Home },
+    { path: 'courses', component: Courses },
+    { path: 'schedule', component: Schedule },
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: '**', component: NotFound }
+];
--- src/app/interfaces/course.ts
+++ src/app/interfaces/course.ts
@@ -1,1 +1,12 @@
-export interface Course {}
+// Interface för kursobjekt från data
+export interface Course {
+    courseCode: string,
+    subjectCode: string,
+    level: string,
+    progression: string,
+    courseName: string,
+    points: number,
+    institutionCode: string,
+    subject: string,
+    syllabus: string
+}
